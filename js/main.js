9fc1eb67f22da97ecad3a017bcff300d73928c73
testgithubbi <123469328+testgithubbi@users.noreply.github.com>
2023-07-13 22:53:38 +0530

    task-2

---
+++ js/main.js
@@ -0,0 +1,75 @@
+
+function show() {
+	var show1=document.querySelector(".dropdown-1").style;
+	if(show1.display=="block"){
+		show1.display="none"
+	}
+	else{
+		show1.display="block"
+	}
+}
+function show1() {
+	var show1=document.querySelector(".dropdown-2").style;
+	if(show1.display=="block"){
+		show1.display="none"
+	}
+	else{
+		show1.display="block"
+	}
+}
+function show2() {
+	var show1=document.querySelector(".dropdown-3").style;
+	if(show1.display=="block"){
+		show1.display="none"
+	}
+	else{
+		show1.display="block"
+	}
+}
+function show3() {
+	var show1=document.querySelector(".dropdown-4").style;
+	if(show1.display=="block"){
+		show1.display="none"
+	}
+	else{
+		show1.display="block"
+	}
+}
+function show4() {
+	var show1=document.querySelector(".dropdown-5").style;
+	if(show1.display=="block"){
+		show1.display="none"
+	}
+	else{
+		show1.display="block"
+	}
+}
+function show5() {
+	var show1=document.querySelector(".dropdown-6").style;
+	if(show1.display=="block"){
+		show1.display="none"
+	}
+	else{
+		show1.display="block"
+	}
+}
+// function show1() {
+// 	document.querySelector(".dropdown-2").style.visibility="visible"
+// 	document.querySelector(".dropdown-2").style.opacity="1"
+// }
+// function show2() {
+// 	document.querySelector(".dropdown-3").style.visibility="visible"
+// 	document.querySelector(".dropdown-3").style.opacity="1"
+// }
+// function show3() {
+// 	document.querySelector(".dropdown-4").style.visibility="visible"
+// 	document.querySelector(".dropdown-4").style.opacity="1"
+// }
+// function show4() {
+// 	document.querySelector(".dropdown-5").style.visibility="visible"
+// 	document.querySelector(".dropdown-5").style.opacity="1"
+// }
+// function show5() {
+// 	document.querySelector(".dropdown-6").style.visibility="visible"
+// 	document.querySelector(".dropdown-6").style.opacity="1"
+// }
